1bb939188e8af96427c328fafd993cc2d65eb66c
navneet <kumar.navneet1@dhani.com>
2023-03-01 00:37:41 +0530

    initial push

---
+++ src/com/Testing/Game.java
@@ -0,0 +1,10 @@
+package com.Testing;
+
+public class Game {
+    private void initializeGame(int boardSize, int playerCount) {
+
+    }
+    public static void Game(int boardSize, int playerCount) {
+
+    }
+}
---
+++ src/com/Testing/Player.java
@@ -0,0 +1,15 @@
+package com.Testing;
+
+import java.util.ArrayList;
+import com.Testing.Board;
+
+public class Player {
+    ArrayList<String> names;
+    public Player(String player1, String player2) {
+        names.add(player1);
+        names.add(player2);
+    }
+    public void move() {
+
+    }
+}
