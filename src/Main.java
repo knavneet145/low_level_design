ecd04c5e6094ca67bfd2552bc4fa36d8e5d0c6e6
navneet <kumar.navneet1@dhani.com>
2024-12-28 23:14:07 +0530

    Adding main and project

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import com.lld.SnakeAndLadder.SnakeAndLadderDemo;
+import com.lld.SocialMedia.Controller.SocialMediaDemo;
+import com.lld.SocialMedia.Service.SocialMediaService;
+import com.lld.Splitwise.SplitwiseDemo;
 import com.lld.StackOverflow.StackOverflowDemo;
 import com.lld.TaskManagement.TaskManagementDemo;
 
@@ -5,7 +9,11 @@ public class Main {
     public static void main(String[] args) {
         TaskManagementDemo taskManagementDemo = new TaskManagementDemo();
         StackOverflowDemo stackOverflowDemo = new StackOverflowDemo();
-        //taskManagementDemo.start();
-        stackOverflowDemo.start();
+        SplitwiseDemo splitwiseDemo = new SplitwiseDemo();
+        SocialMediaDemo socialMediaDemo = new SocialMediaDemo();
+        SnakeAndLadderDemo snakeAndLadderDemo = new SnakeAndLadderDemo();
+
+        Thread thread = new Thread(snakeAndLadderDemo);
+        thread.start();
     }
 }
